dff02e9900b01fcf4584b614e948355cad9afe3e
YanaTestM <yimyana@yandex.ru>
2023-02-02 00:49:56 +0300

    telebot

---
+++ telebot/config.py
@@ -0,0 +1,2 @@
+TOKEN = '6038871640:AAEPvuI2pAGh3wfWjkfVJFhBxIc5NC-ADpU'
+keys = {'евро': 'EUR', 'рубль': 'RUR', 'доллар': 'USD'}
---
+++ telebot/main.py
@@ -0,0 +1,46 @@
+import telebot
+from config import keys, TOKEN
+from extensions import ConvertionException, CryptoConverter
+
+bot = telebot.TeleBot(TOKEN, parse_mode=None)
+
+
+# обработчик команд [старт, хелп]
+@bot.message_handler(commands=['start', 'help'])
+def help(message: telebot.types.Message):
+    text = 'Чтобы начать работу введите команду боту в следующем формате:\n<имя валюты> ' \
+           '<в какую валюту перевести> ' \
+           '<количество переводимой валюты>'
+    bot.reply_to(message, text)
+
+
+# Обработчик команды [values]
+@bot.message_handler(commands=['values'])
+def values(message: telebot.types.Message):
+    text = 'Доступные валюты:'
+    for key in keys.keys():
+        text = '\n'.join((text, key,))
+    bot.reply_to(message, text)
+
+
+# Обработчик текста
+@bot.message_handler(content_types=['text'])
+def convert(message: telebot.types.Message):
+    try:
+        values = message.text.lower().split(' ')
+
+        if len(values) != 3:
+            raise ConvertionException('Не верное количество параметров.')
+
+        quote, base, amount = values
+        total_base = CryptoConverter.get_price(quote, base, amount)
+    except ConvertionException as e:
+        bot.reply_to(message, f'Ошибка пользователя\n{e}')
+    except Exception as e:
+        bot.reply_to(message, f'Не удалось обработать команду\n{e}')
+    else:
+        text = f'Цена {amount} {quote} в {base} - {total_base}'
+        bot.send_message(message.chat.id, text)
+
+
+bot.infinity_polling()
